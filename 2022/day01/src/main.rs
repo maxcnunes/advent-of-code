afd77983e65adf84318c700cfebfadaa179de2cd
Max Claus Nunes <maxcnunes@gmail.com>
2023-10-20 21:36:48 -0300

    Make solution more efficient

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
-use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() {
     let file_name = match env::var("DEMO") {
@@ -9,22 +11,24 @@ fn main() {
 
     println!("Using input data {}", file_name);
 
-    let lines = read_lines(file_name);
+    let lines = read_lines(file_name).unwrap();
 
-    let mut elf_most_calories = 0;
-    let mut total_most_calories = 0;
+    let mut elf_most_calories: u8 = 0;
+    let mut total_most_calories: u32 = 0;
 
-    let mut current_elf = 1;
-    let mut current_total_calories = 0;
+    let mut current_elf: u8 = 1;
+    let mut current_total_calories: u32 = 0;
 
-    for line in lines {
+    for line_result in lines {
+        let line = line_result.unwrap();
+
         if line == "" {
             current_elf += 1;
             current_total_calories = 0;
             continue;
         }
 
-        let calories: i32 = line.parse().unwrap();
+        let calories: u32 = line.parse().unwrap();
         current_total_calories += calories;
 
         if current_total_calories > total_most_calories {
@@ -32,16 +36,19 @@ fn main() {
             total_most_calories = current_total_calories;
         }
     }
+
     println!(
         "The elf {} is carrying the most calories {}.",
         elf_most_calories, total_most_calories
     );
 }
 
-fn read_lines(filename: &str) -> Vec<String> {
-    read_to_string(filename)
-        .unwrap() // panic on possible file-reading errors
-        .lines() // split the string into an iterator of string slices
-        .map(String::from) // make each slice into a string
-        .collect() // gather them together into a vector
+// Uses a buffer and iterator to read a file instead of loading the whole
+// file in memory at once.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
